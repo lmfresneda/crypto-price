c3b3dc4a6ca5199a1c9600fe96e500f266160498
Luis Miguel F <miguelfr.reg@gmail.com>
2017-12-29 00:51:34 +0100

    change url to app config

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,3 +1,3 @@
 export default {
-  url_config: 'https://gist.githubusercontent.com/lmfresneda/794c2f49ac7a63dd3ca7c29ef87ab8f0/raw/3f831a9c7cc898be35656bfa94d0fb2264c7ab72/config_crypto_app.json'
+  url_config: 'https://raw.githubusercontent.com/lmfresneda/crypto-price/master/config-app.json'
 }
